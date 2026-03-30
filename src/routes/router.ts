781786190fa997ef84c3f02e6cd31edb07c3c052
shubham94github <phanases@gmail.com>
2026-03-30 15:58:11 +0530

    v4

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -2,7 +2,7 @@ import { Router } from "express";
 
 const router = Router();
 
-router.get("/", (req, res) => res.json({ message: "Hello from Docker v3 🎉" }));
+router.get("/", (req, res) => res.json({ message: "Hello from Docker v4 🎉" }));
 
 router.get("/health", (req, res) => {
   //  throw new Error("Internal Server Error");
